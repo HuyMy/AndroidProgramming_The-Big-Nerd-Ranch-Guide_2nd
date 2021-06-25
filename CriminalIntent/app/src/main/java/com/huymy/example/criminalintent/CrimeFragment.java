6c8638000517575606ab8ff1538a450663c280ea
HuyMy <lqhuy91@gmail.com>
2021-06-25 14:11:15 +0700

    Create crime fragment and and it to CrimeActivity.

---
+++ CriminalIntent/app/src/main/java/com/huymy/example/criminalintent/CrimeFragment.java
@@ -0,0 +1,48 @@
+package com.huymy.example.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleFieldText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        mTitleFieldText = (EditText) v.findViewById(R.id.crime_title_text);
+        mTitleFieldText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        return v;
+    }
+}
