d4a5769f2e544f013a4a0905b157bccd4e70135a
HuyMy <lqhuy91@gmail.com>
2022-01-03 18:13:36 +0700

    Use SQLite Databases to store crimes.

--- CriminalIntent/app/src/main/java/com/huymy/example/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/huymy/example/criminalintent/CrimeListFragment.java
@@ -107,6 +107,7 @@ public class CrimeListFragment extends Fragment {
             mCrimeAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mCrimeAdapter);
         } else {
+            mCrimeAdapter.setCrimes(crimes);
             mCrimeAdapter.notifyDataSetChanged();
         }
         updateSubtitle();
@@ -165,6 +166,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
     }
 }
